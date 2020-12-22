e715d3ee523c66149c02df2c292980dd400ad9d1
liuzhuanwei <liuzhuanwei@amii.com>
2020-12-22 13:46:57 +0800

    订单服务：确认订单流程实现-2020-12-22

--- shop-common/src/main/java/com/liudao51/shop/common/constant/AppCode.java
+++ shop-common/src/main/java/com/liudao51/shop/common/constant/AppCode.java
@@ -15,12 +15,21 @@ public enum AppCode {
 
     //请求格式
     REQUEST_FORMAT_JSON(1, "json", "json格式"),
-    REQUEST_FORMAT_XML(2, "xml", "xml格式"),
+    REQUEST_FORMAT_JSON_2(2, "JSON", "JSON格式"),
+    REQUEST_FORMAT_XML(1, "xml", "xml格式"),
+    REQUEST_FORMAT_XML_2(2, "XML", "XML格式"),
+    REQUEST_CONTENT_TYPE_TEXT(1, "text/plain", "Content-Type:text/plain类型"),
+    REQUEST_CONTENT_TYPE_JSON(2, "application/json", "Content-Type:application/json类型"),
+    REQUEST_CONTENT_TYPE_XML(3, "application/xml", "Content-Type:application/xml类型"),
+    REQUEST_CONTENT_TYPE_JAVASCRIP(4, "application/javascrip", "Content-Type:application/javascrip类型"),
 
     //字符编码
     CHARSET_UTF8(1, "utf-8", "utf-8格式"),
-    CHARSET_GBK(2, "gbk", "gbk格式"),
-    CHARSET_GB2312(3, "gb2312", "gb2312格式"),
+    CHARSET_UTF8_2(2, "UTF-8", "UTF-8格式"),
+    CHARSET_GBK(1, "gbk", "gbk格式"),
+    CHARSET_GBK_2(2, "GBK", "GBK格式"),
+    CHARSET_GB2312(1, "gb2312", "gb2312格式"),
+    CHARSET_GB2312_2(2, "GB2312", "GB2312格式"),
 
     //订单状态
     ORDER_ORDER_STATUS_NO_CONFIRM(0, "NO_CONFIRM", "订单未确认"),
